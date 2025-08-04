02f682cd10c6605c974ce11ee10f6007050c9017
panda3diitx <panda3diitx@gmail.com>
2025-08-04 19:07:02 +0500

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ ANTI_LINK: process.env.ANTI_LINK || "true",
 // make anti link true,false for groups 
 MENTION_REPLY: process.env.MENTION_REPLY || "false",
 // make true if want auto voice reply if someone menetion you 
-MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/qpnazl.jpg",
+MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/u23u54.jpg",
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
@@ -49,7 +49,8 @@ DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ 𝐍ᴀɪ
 // add bot owner name    
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/u23u54.jpg",
 // add img for alive msg
-LIVE_MSG: process.env.LIVE_MSG || "> Zinda Hun Yar ⚡",
+LIVE_MSG: process.env.LIVE_MSG || "*📍-𝐍͡ᴀɪᴍᴀ 𝐓ᴇᴄʜ͡𝐗  ›*
+*_سَتَا دَلَاسَہ مِی٘ زَڑ٘گِے زَرِی زَرِی :____*",
 // add alive msg here 
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
