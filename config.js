7844101efda6f843a9a8abc7b3176e159634417e
panda3diitx <panda3diitx@gmail.com>
2025-08-04 18:57:26 +0500

    Update config.js

--- config.js
+++ config.js
@@ -31,7 +31,7 @@ MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/qpnazl.j
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "Your-Own-Bot",
+BOT_NAME: process.env.BOT_NAME || "𝐍͡ᴀɪᴍᴀ 𝐓ᴇᴄʜ͡𝐗",
 // add bot namw here for menu
 STICKER_NAME: process.env.STICKER_NAME || "*𝐍ᴀɪᴍᴀ 𝐓ᴇᴄʜ𝐗   :)*",
 // type sticker pack name 
